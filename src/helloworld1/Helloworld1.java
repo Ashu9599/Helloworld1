ec2c05c4668ca2d3fdc8bbb91123ad6a9d507b69
Ashu9599 <ashutoshdas091@gmail.com>
2019-07-28 20:19:50 +0530

    first commit

---
+++ src/helloworld1/Helloworld1.java
@@ -0,0 +1,15 @@
+//ashutosh das program for print hello 
+package helloworld1;
+
+public class Helloworld1 {
+	/**
+     * 015 @param args
+     */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 System.out.println("Hello World");
+		 //ashutosh das for print
+
+	}
+
+}
